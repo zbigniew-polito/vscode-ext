03ee0ca6098e47fa42ca5284c55b5f03f92daefe
beamy <git@localhost>
2023-04-10 18:31:02 +0200

    autocommit

--- src/extension.ts
+++ src/extension.ts
@@ -31,7 +31,6 @@ function print(msg: string | any) {
 
 }
 
-
 function getProperty<T, K extends keyof T>(obj: T, key: string): T[K] {
 	if (key in obj) { return obj[key as K]; }
 	throw new Error(`Invalid object member "${key}"`);
