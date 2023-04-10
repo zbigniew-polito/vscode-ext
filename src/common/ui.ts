ed8a1b719eb23ab24ce2eea9638173b97ef7eff2
beamy <git@localhost>
2023-04-11 00:32:01 +0200

    autocommit

--- src/common/ui.ts
+++ src/common/ui.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
-import print from "./printer"
+import print from "./printer";
 
 function success(msg: any) {
-    print(msg)
+    print(msg);
     vscode.window.showInformationMessage(msg);
     return true;
 }
