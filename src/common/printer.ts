f36ff4e953922959b8cb0dd06be8612a3a986b0b
beamy <git@localhost>
2023-04-10 17:54:01 +0200

    autocommit

--- src/common/printer.ts
+++ src/common/printer.ts
@@ -5,14 +5,14 @@ import { OutputChannel } from "vscode";
 //const dictionary = new Map<string, string>();
 
 class Printer {
-    public history: Map<string, (string | object | undefined)[]> = new Map<string, (string | object | undefined)[]>();
+    public history: Map<string | undefined, (string | object | undefined)[]> = new Map<string, (string | object | undefined)[]>();
 
     constructor() {
 
     }
     public print(...args: any, channel: string | undefined) {
         //channel = channel ? channel : "default";
-        this.history.get(channel).
+        this.history.get(channel) ??
     }
 }
 
