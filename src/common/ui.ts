b6455f6dd765b93709a2c2d87a33b5810713ce63
beamy <git@localhost>
2023-04-12 06:13:01 +0200

    autocommit

--- src/common/ui.ts
+++ src/common/ui.ts
@@ -1,13 +1,13 @@
 import * as vscode from "vscode";
 import print from "./printer";
 
-function success(msg: any) {
+export function success(msg: any) {
 	print(msg);
 	vscode.window.showInformationMessage(msg);
 	return true;
 }
 
-function error(msg: any) {
+export function error(msg: any) {
 	print(msg);
 	vscode.window.showErrorMessage(msg);
 	return false;
