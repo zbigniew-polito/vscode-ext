478acbdb0dc34dc50f83651619423d6f65f6baba
beamy <git@localhost>
2023-04-15 23:47:01 +0200

    autocommit

--- src/VsCode.ts
+++ src/VsCode.ts
@@ -1,3 +1,9 @@
-class VsCode implements Provider {}
+import * as vscode from "vscode";
+
+class VsCode implements Provider {
+	public get config(): StringByString {
+		return vscode.workspace.getConfiguration()?.get("pyutils") ?? {};
+	}
+}
 
 export default VsCode;
